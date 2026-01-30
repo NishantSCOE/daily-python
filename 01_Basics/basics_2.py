0ca2837f96e1a5211caa64cfbafb1a89a3397f45
Nishant Deshmukh <nishantdeshmukh996@gmail.com>
2026-01-30 21:13:57 +0530

    Practice

---
+++ 01_Basics/basics_2.py
@@ -0,0 +1,18 @@
+# single lin comment 
+
+'''
+Docstring for DAILY-PYTHON.daily-python.01_Basics.basics_2
+'''
+
+def add(x,y):
+    """
+    Docstring for add
+    
+    :param x: first number
+    :param y: second number
+
+    this functions adds two nubers and print the answer
+    """
+    print("Sum=",(x+y))
+
+add(2,3)
