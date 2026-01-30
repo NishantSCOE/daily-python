39fe60ea7b1bd12f08390354a96919e59eccf2a5
Nishant Deshmukh <nishantdeshmukh996@gmail.com>
2026-01-30 21:23:44 +0530

    Practice

---
+++ 01_Basics/basics_3.py
@@ -0,0 +1,11 @@
+def fun(x,y,z):
+    if x>y and x>z:
+        print("X is greater than Y and Z")
+    if y>x and y>z:
+        print("Y is greater than X and Z")
+    if z>x and z>y:
+        print("Z is greater than X and Y")
+
+    print("Function completed")
+
+fun(3,56,71)
